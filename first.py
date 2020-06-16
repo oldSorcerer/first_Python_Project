cf07070270486ea3e9c153c5327e870798da7abc
aleksei.derevyannykh@gmail.com <Echorupta0habibi0viki>
2020-06-16 18:28:50 +0300

    Initial commit Start project

---
+++ first.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def go():
+    filename = 'programm.csv'
+    with open(filename, 'w') as file_object:
+        file_object.write('name')
+        file_object.write(',')
+        file_object.write('email')
+        file_object.write(',')
+        file_object.write('answer')
+        file_object.write("\n")
+    return render_template('1.html')
+
+@app.route('/answer', methods=['POST'])
+def nn():
+    name = request.form['name']
+    email = request.form['email']
+    answer = request.form['message']
+    filename = 'programm.csv'
+
+    with open(filename, 'a') as file_object:
+        file_object.write(name)
+        file_object.write(',')
+        file_object.write(email)
+        file_object.write(',')
+        file_object.write(answer)
+        file_object.write("\n")
+
+    return render_template('1.html')
+
+if __name__ == '__main__':
+       app.run(debug=True)
